dadc30595574fbe28cf6e7374e01dbb4df2551e8
Голдобина Светлана Юрьевна <sgoldobina@alfabank.ru>
2017-10-11 00:28:45 +0300

    sm fix names

---
+++ ReverseWords/ReverseWords.py
@@ -0,0 +1,8 @@
+while True:
+    input_str = input("\nTo exit enter 0\nOr enter two words separated by a space: ")
+
+    if input_str.isdigit() and int(input_str) == 0: exit(0)
+    elif len(input_str.split()) != 2:
+        print("Try again. Enter two words separated by a space.")
+        continue
+    else: print(' '.join(input_str.split()[::-1]))
