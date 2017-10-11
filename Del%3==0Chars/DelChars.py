4db47a25ad61df8da4794fb50b0ecc25b75c0c43
Голдобина Светлана Юрьевна <sgoldobina@alfabank.ru>
2017-10-11 10:16:29 +0300

    delete characters, which index % 3 == 0

---
+++ Del%3==0Chars/DelChars.py
@@ -0,0 +1,18 @@
+# Goldobina Svetlana
+#
+# Delete the character which index %3 == 0
+
+while True:
+    # input string
+    str = input("\nTo exit enter 0\nOr enter a string: ")
+
+    # if not one word
+    if len(str.split()) != 1:
+        print("Enter one word, try again")
+        continue
+
+    # if want to exit
+    elif str.isdigit() and int(str) == 0: exit(0)
+
+    # del characters
+    print(''.join([x for x in str if str.index(x) % 3 != 0]))
