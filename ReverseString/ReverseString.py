bb609afb13fe43267578122aa65835ea753e2f82
Голдобина Светлана Юрьевна <sgoldobina@alfabank.ru>
2017-10-11 11:10:00 +0300

    name fix

--- ReverseString/ReverseString.py
+++
@@ -1,8 +0,0 @@
-while True:
-    input_str = input("To exit enter 0\nOr enter two words separated by a space: ")
-
-    if input_str.isdigit() and int(input_str) == 0: exit(0)
-    elif len(input_str.split()) != 2:
-        print("Try again. Enter two words separated by a space: ")
-        continue
-    else: print(' '.join(input_str.split()[::-1]))
