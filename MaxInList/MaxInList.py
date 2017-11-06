03eb03fdf7c66524cdaec490a1a78a30e8bb730f
Голдобина Светлана Юрьевна <sgoldobina@alfabank.ru>
2017-11-06 03:40:03 +0300

    max elen

---
+++ MaxInList/MaxInList.py
@@ -0,0 +1,32 @@
+# Goldobina Svetlana
+#
+# find max element and the last its index
+while True:
+    try:
+        # input a list
+        lst = input("(To close the app input \"exit\")\n"
+                    "Enter an array of numbers without common and press enter: ").split()
+
+        # exit mode
+        if lst[0] == "exit": exit(0)
+
+        # choose numbers
+        lst = list(filter(lambda x: x.isdigit(), lst))
+
+        # set started values of number and index
+        max = lst[0]
+        max_index = 0
+
+        # find max and its the last index
+        for i in range(0, len(lst)):
+            if lst[i] and lst[i] >= max:
+                max = lst[i]
+                max_index = i
+
+        # print result
+        print("value: ", max, "index = ", max_index)
+
+    # empty list, list of chars, punctuations
+    except IndexError:
+        print("Valid error, try again\n")
+        continue
