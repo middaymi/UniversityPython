6cf309500d70a8aaa6665e8787c6691ef17a84d3
Голдобина Светлана Юрьевна <sgoldobina@alfabank.ru>
2017-11-22 00:57:05 +0300

    del a list

--- LocalMax/LocalMax.py
+++ LocalMax/LocalMax.py
@@ -2,44 +2,55 @@
 #
 # find local maximums in an array
 # local maximum - the number which is greater than its neighbor
-# border values ​​are compared with one neighbor
+# border values â€‹â€‹are compared with one neighbor
 
+
+# print result and clear data
+def print_clear_data(lst, dis):
+    if len(lst) != 0:
+        print("list:", lst)
+        if len(dis) != 0:
+            print("distance list:", dis,
+                  "\nmin distance:", min(dis))
+            dis.clear()
+        else:
+            print("min distance:", 0)
+        lst.clear()
+
+
+# find min distance between neighbors
+def find_min_distance(lst, dis, index):
+    for i in range(1, len(lst) - 1):
+        if lst[i - 1] < lst[i] > lst[i + 1]:
+            if i != 1:
+                dis.append(i - index)
+            index = i
+
+
+# getting a list as str from keyboard
+def input_check_array(lst):
+    print("\nEnter values:")
+    while True:
+        current = input().strip()
+        # exit mode
+        if current.strip() == "exit":
+            exit(0)
+
+        if int(current) != 0:
+            lst.append(int(current))
+        else:
+            break
+
+
+array = []
+distance = []
+index_of_localmax = 0
 
 while True:
-
-    # getting a list as str from keyboard
-    lst = (input("\nFor exit enter 0 \nOr enter an array of natural numbers: ").strip().split())
-
-    # exit mode
-    if lst[0] == '0': exit(0)
-
     try:
-        # to do an array of numeric values
-        lst = list(map(int, lst))
-        print(lst)
-        ll = len(lst)
-
-        # create an array of the local max indexes
-        index_lst = []
-
-        # find local maximums and add them to the array of indexes
-        for i in range(0, ll):
-            if i == 0 and lst[0] > lst[1]: index_lst.append(0)
-            elif i == ll - 1 and lst[ll - 1] > lst[ll - 2]: index_lst.append(ll - 1)
-            elif lst[i - 1] < lst[i] > lst[i + 1]: index_lst.append(i)
-        print(index_lst)
-        li = len(index_lst)
-
-        # find min range of the local maximums indexes
-        if li != 1:
-            min_range = index_lst[1] - index_lst[0]
-            for i in range(1, li):
-                tmp = index_lst[i] - index_lst[i - 1]
-                if tmp < min_range: min_range = tmp
-        # if one number range = 0
-        else:
-            min_range = 0
-        print(min_range)
+        input_check_array(array)
+        find_min_distance(array, distance, index_of_localmax)
+        print_clear_data(array, distance)
 
     except ValueError:
-        print("Incorrect values! Try again!")
+        print("Enter numbers! Try again.")
